0a6e3393dec6677fe0347b618d110493e0b9d761
rogeriolippert <rogeriolippert35@gmail.com>
2017-03-28 20:56:28 -0300

    inclusão novos teste

--- calc.spec.js
+++ calc.spec.js
@@ -9,3 +9,25 @@ lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
     Code.expect(soma(2, 3)).to.equal(5);
     done();
 });
+
+
+
+lab.test('retorno da soma de 8 e 3 deve ser 11', (done) => {
+
+    Code.expect(soma(8, 3)).to.equal(11);
+    done();
+});
+
+
+
+lab.test('retorno da soma de -4 e 3 deve ser 1', (done) => {
+
+    Code.expect(soma(-4, 3)).to.equal(-1);
+    done();
+});
+
+lab.test('retorno da soma de -4 e -3 deve ser -7', (done) => {
+
+    Code.expect(soma(-4, 3)).to.equal(-7);
+    done();
+});
